c2e478f6a66dd2c795f9a00e1233092d6b50b3b8
Brad Campbell <bradjc5@gmail.com>
2023-08-15 08:09:46 -0400

    build/make: set the page size
    Otherwise lld seems to assume 0x10000 sized pages and tries to align the
    output segments to 0x10000. This often causes elfs with large padding
    regions before the first segment we actually want to load on the board.

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
 fn main() {
+    // Support for fixed-address compilation.
     libtock_build_scripts::auto_layout();
+
+    // Configure the alignment size for the linker. This prevents the linker
+    // from assuming very large pages (i.e. 65536 bytes) and unnecessarily
+    // inserting additional padding into the output ELF.
+    println!("cargo:rustc-link-arg=-zmax-page-size=4096");
 }
